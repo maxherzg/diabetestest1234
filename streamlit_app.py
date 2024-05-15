09c77b677046980789010d9e001ad4eca33f1d72
maxherzg <144324266+maxherzg@users.noreply.github.com>
2024-05-15 15:42:29 +0000

    hjh

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,8 +67,8 @@ Sex = st.radio('Sex', options=['Male', 'Female'])
 sex_numeric = 1 if Sex == 'Male' else 0
 Age = st.number_input('Age', min_value=0, step=1)
 
-Education = st.selectbox('Education Level', options=['Less than High School', 'High School Graduate', 'College', 'Graduated School'])
-education_numeric = {'Less than High School': 1, 'High School Graduate': 2, 'College': 3, 'Graduated School': 4}[Education]
+Education = st.selectbox('Education Level', options=['Less than High School', 'High School Graduate', 'Bachlores Degree', 'Masters Degree'])
+education_numeric = {'Less than High School': 1, 'High School Graduate': 2, 'Bachlores Degreee': 3, 'Masters Degree': 4}[Education]
 
 Income = st.selectbox('Income Level', options=['Less than $10,000', '$10,000 to $24,999', '$25,000 to $49,999', '$50,000 to $74,999', '$75,000 or more'])
 income_numeric = {'Less than $10,000': 1, '$10,000 to $24,999': 2, '$25,000 to $49,999': 3, '$50,000 to $74,999': 4, '$75,000 or more': 5}[Income]
